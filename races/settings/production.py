7b091c9f6659f858c4d21f3055147fe9bc7c4443
Steve Cassidy <steve.cassidy@gmail.com>
2013-06-04 21:44:39 +1000

    Add static_root

--- races/settings/production.py
+++ races/settings/production.py
@@ -13,3 +13,13 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# should be customised on the production server and not kept in VC
+SECRET_KEY = '^g=q21r_nnmbz49d!vs*2gvplfsd((02l-y9b@&amp;t3k2r3c$*u&amp;2la5!%s'
