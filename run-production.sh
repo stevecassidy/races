5df7a1d7cbc9ef3b597a78cc7b29c72e85d209f6
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-12-30 21:51:02 +1100

    Add updated run script

--- run-production.sh
+++ run-production.sh
@@ -1,1 +1,4 @@
-docker run -d --env-file env.prod -p 8000:8000 --restart unless-stopped  docker.pkg.github.com/stevecassidy/races/web:latest gunicorn -c ./gunicorn.config.py races.wsgi
+docker pull docker.pkg.github.com/stevecassidy/races/web:latest
+docker stop cabici
+docker rm cabici
+docker run -d --env-file env.prod -p 8000:8000   --restart unless-stopped  --name cabici docker.pkg.github.com/stevecassidy/races/web:latest gunicorn -c ./gunicorn.config.py  races.wsgi
