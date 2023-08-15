fca236c8952536768ea580b1e3f893fe744d1520
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-08-15 00:48:23 +0000

    remove old secrets
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- races/settings/development.py
+++ races/settings/development.py
@@ -18,36 +18,9 @@ from races.settings.base import *
 
 DATABASES['default']['NAME'] = 'races_development'
 
-SOCIAL_AUTH_TWITTER_KEY = 'gx3LnhtWJyFbyH3bt6wR18u2j'
-SOCIAL_AUTH_TWITTER_SECRET = 'SQTGvdCnhopWW6nQiCB0GozOrQSB9dvBESjif6D5xq0FkVg2gM'
-
-SOCIAL_AUTH_STRAVA_KEY = '5805'
-SOCIAL_AUTH_STRAVA_SECRET = '518be609d5f852ef1bbb64d2017afc1a5b8c71f2'
-
-# test.cabici.net config, main site is different
-SOCIAL_AUTH_FACEBOOK_KEY = '212693232435594'
-SOCIAL_AUTH_FACEBOOK_SECRET = '01acd83d40e996f60386f9a87454b04c'
-
-
 # email to the console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-## django-anymail configuration
-#ANYMAIL = {
-#    "MAILGUN_API_KEY": 'key-579a20de44b97c7dd72e13be867acb70',
-#    "MAILGUN_SENDER_DOMAIN": 'mail.cabici.net'
-#}
-#EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 
-
-#GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyDnUgACSI5LnSOS5hDsR-iWiZ5ARzur_Bs'
-GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyCs0yGI4aREzy8Ej3zuulAsoPKtXBiSXRM'
+GEOPOSITION_GOOGLE_MAPS_API_KEY = 'something'
 GOOGLE_MAPS_API_KEY = GEOPOSITION_GOOGLE_MAPS_API_KEY
-
-
-
-PINAX_WEBANALYTICS_SETTINGS = {
-    "google": {
-        1: "UA-41522408-1", # production
-    },
-}
