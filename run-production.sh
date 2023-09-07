ee446c8e8f46e2eb45d09abd121990cbe5e5ab33
Evgeny Chernyshov <evgeny.chernyshov@hkeeper.global>
2023-09-07 11:59:19 +0200

    docker image renamed

--- run-production.sh
+++ run-production.sh
@@ -1,4 +1,4 @@
-docker pull waratahmasters/races:latest
+docker pull waratahmasters.races:latest
 docker stop cabici
 docker rm cabici
-docker run -d --env-file env.prod -p 8000:8000   --restart unless-stopped  --name cabici waratahmasters/races:latest gunicorn -c ./gunicorn.config.py  races.wsgi
+docker run -d --env-file env.prod -p 8000:8000   --restart unless-stopped  --name cabici waratahmasters.races:latest gunicorn -c ./gunicorn.config.py  races.wsgi
