946adf70a1c7fdab0e3f6d52be5f3f3874e39b84
accompany1205 <accompaniedwithyou@gmail.com>
2024-02-06 21:11:32 -0800

    club rider test

--- races/apps/cabici/views.py
+++ races/apps/cabici/views.py
@@ -254,12 +254,12 @@ class ClubRidersView(ListView):
     def post(self, request, **kwargs):
         """Handle upload of membership spreadsheets"""
         slug = self.kwargs['slug']
-        return HttpResponse(slug.__str__())
         club = Club.objects.get(slug=slug)
         form = MembershipUploadForm(request.POST, request.FILES)
         
         if form.is_valid():
             mf = request.FILES['memberfile']
+            return HttpResponse(len(mf).__str__());
             try:
                 changed = Rider.objects.update_from_tidyhq_spreadsheet(club, codecs.iterdecode(mf, 'utf-8'))
                 return HttpResponse(changed.__str__())
